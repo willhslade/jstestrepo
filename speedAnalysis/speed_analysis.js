bebb70a1b5e498198aed131b04d1cc4b1eea971f
William Slade <willhslade@gmail.com.com>
2024-10-18 17:18:58 -0400

    seventh commit from Coursera

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -39,8 +39,9 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
-        "<p>Words Typed: " + typedWords + "</p>" +
-        "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
+    "<p>Total Length: " + userTypedText.length + "</p>" +
+    "<p>Words Typed: " + typedWords + "</p>" +
+    "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
 
     // Reset the button
