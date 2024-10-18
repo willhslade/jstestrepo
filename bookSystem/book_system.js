94fb59b344e92bad680f1f2b249cc09e03f0c343
William Slade <willhslade@gmail.com.com>
2024-10-18 11:56:38 -0400

    fifth coursera commit - deleteBook

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <p><button onclick="deleteBook(${index})">Delete Book</button></p>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -35,4 +36,10 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(index) {
+    books.splice(index, 1);
+    showbooks();
+    clearInputs();
 }
